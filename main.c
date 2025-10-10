24c72238fb39f8f9a065c9fbe55b9cfcdc0b0874
huywallz <huywall69@gmail.com>
2025-10-10 22:04:23 +0700

    Testing Static Library

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tug.h"
+
+static char* read_file(const char* path) {
+    FILE* file = fopen(path, "r");
+    if (!file) return NULL;
+
+    fseek(file, 0, SEEK_END);
+    long size = ftell(file);
+    rewind(file);
+
+    char* buf = malloc(size + 1);
+    fread(buf, size, 1, file);
+    buf[size] = '\0';
+
+    fclose(file);
+    return buf;
+}
+
+int main() {
+    tug_init();
+
+    char errmsg[2048];
+    char* code = read_file("main.tug");
+    tug_Task* task = tug_task("main.tug", code, errmsg);
+    free(code);
+    if (!task) {
+        printf("%s\n", errmsg);
+        return 1;
+    }
+    tug_resume(task);
+
+    tug_close();
+    return 0;
+}
